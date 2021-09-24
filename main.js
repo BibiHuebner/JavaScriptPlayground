767b4de873c3b1ff0c076431a88f6a4090ef8647
BibiHuebner <geffers.annika@gmail.com>
2021-09-24 14:31:37 +0200

    Ex3done

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Starting javascript...");
+var myName = "Annika";
+console.log(myName);
+var myAge = 37;
+console.log(myAge);
+var juliaAge = 32;
+var ageDiff = myAge - juliaAge
+console.log(ageDiff);
+var age = 21;
+if (myAge > age) { console.log("You are older than 21"); }
+else { console.log("You are not older than 21");}
