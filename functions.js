d98c191e92e6e5425eed1434e0a7e23da7ec244c
BibiHuebner <geffers.annika@gmail.com>
2021-09-24 14:34:29 +0200

    Exercise 3 done

---
+++ functions.js
@@ -0,0 +1,14 @@
+function sayHello() {
+    console.log("Hello World");
+}
+sayHello(); //*so muss ich nicht console.log schreiben//*
+
+function greetSomeone(msg, person) {
+    console.log()
+}
+
+
+//for exercises just use: for loops, //while loop//if, else if, else conditions/ functions//
+// .sort() um arrays zu sorten
+// don't use .max and .min, try to use sorting .sort() and functions and solve problem logically
+// .split() and .join() are allowed
