9f36dec2bcc6c35e900b35c39be25bed67526c38
BibiHuebner <geffers.annika@gmail.com>
2021-09-24 15:30:32 +0200

    Exercise 5 completed I have a question on if else if

--- main.js
+++ main.js
@@ -9,3 +9,6 @@ console.log(ageDiff);
 var age = 21;
 if (myAge > age) { console.log("You are older than 21"); }
 else { console.log("You are not older than 21");}
+if (juliaAge > myAge) { console.log("Julia is older than you"); }
+else if (juliaAge < myAge) { console.log("Julia is younger than you") }
+else { console.log("You have the same age as Julia") }
